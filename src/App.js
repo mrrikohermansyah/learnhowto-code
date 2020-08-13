ee6903cae329634462dde0681391e3fb57e19f31
Riko Hermansyah <mr.rikohermansyah@gmail.com>
2020-08-13 16:00:55 +0700

    Coba JS Counter

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Counter App</h1>
+        <div>
+          <Counter initialNumber ={0} />
+          <Counter initialNumber ={-10} />
+          <Counter initialNumber ={5} />
+          <Counter initialNumber ={30} />
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
